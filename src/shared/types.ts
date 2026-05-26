8612234952c620fa5faf70627d771efc949893a5
Konstantina Melikoki <konstantina.melik@gmail.com>
2026-05-26 21:50:35 +0300

    feat(ui): add Button shared component

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,6 +1,18 @@
-import React from "react";
-
 export interface LayoutProps {
     children: React.ReactNode;
+    addClasses?: string;
+}
+
+export type ButtonProps = {
+    onClick?: () => void;
+    disabled?: boolean;
+    label: string;
+    addClasses?: string;
+}
+
+export type IconButtonProps = {
+    onClick?: () => void;
+    disabled?: boolean;
+    icon: React.ReactNode;
     addClasses?: string;
 }
